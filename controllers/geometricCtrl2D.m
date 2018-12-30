171d0bfccedbda66bd72d3660c137011bbca80ba
JaeyoungLim <gokoreas@gmail.com>
2018-12-30 19:20:14 +0100

    define controller as a function
    This can serve as a library of controllers

---
+++ controllers/geometricCtrl2D.m
@@ -0,0 +1,22 @@
+function [w, thrust] = geometricCtrl2D(obj, ref)
+    [position, attitude, velocity] = obj.getstate();
+    
+    error_pos = position - ref;
+    error_vel = velocity - [0.0, 0.0];
+    
+    %% Calculate desired acceleration
+    Kp = -2.0;
+    Kv = -9.0;
+
+    desired_acc = Kp * error_pos + Kv * error_vel + [0.0, 9.8];
+    
+    %% Calculate desired angular velocity
+    tau = 0.1;
+    
+    desired_att = atan2(desired_acc(2), desired_acc(1)) - 0.5 * pi();
+    
+    error_att = desired_att - attitude;
+    w = (1/tau) * error_att;
+    thrust = obj.m * norm(desired_acc);
+
+end
