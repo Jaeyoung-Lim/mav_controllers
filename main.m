8d29efdf97d22535d1a2ef1896020bbbe2dcff28
JaeyoungLim <gokoreas@gmail.com>
2018-12-30 18:44:00 +0100

    Geometric controller now flying

--- main.m
+++ main.m
@@ -3,25 +3,55 @@
 
 %% Initialize
 clc; clear all;
+pause on
+figure(1);
+dt = 0.01;
 
+ref = [0.0, 1.0];
+
+%%
 pos = [];
 loop = 0;
 
-w = 0.1;
-thrust = 1.0;
-
-dt = 0.01;
-
+% Initialize model
 model = MODEL_2D_Multirotor();
 
-figure(1);
 while true
-
-    model = model.step(w, thrust, dt);
-    model.visualize();
-    loop = loop + 1;
+   [w, thrust] = geometricCtrl2D(model, ref);
+   model = model.step(w, thrust, dt);
+   loop = loop + 1;
     pos = model.getstate();
+    if loop > 5
+        loop = 0;
+        model.visualize();
+    end
+
     if islanded(pos)
        break; 
     end
+    pause(dt);
+
+end
+
+function [w, thrust] = geometricCtrl2D(obj, ref)
+    [position, attitude, velocity] = obj.getstate();
+    
+    error_pos = position - ref;
+    error_vel = velocity - [0.0, 0.0];
+    
+    %% Calculate desired acceleration
+    Kp = -2.0;
+    Kv = -9.0;
+
+    desired_acc = Kp * error_pos + Kv * error_vel + [0.0, 9.8];
+    
+    %% Calculate desired angular velocity
+    tau = 1;
+    
+    desired_att = atan2(desired_acc(2), desired_acc(1)) - 0.5 * pi();
+    
+    error_att = desired_att - attitude;
+    w = (1/tau) * error_att;
+    thrust = obj.m * norm(desired_acc);
+
 end
--- model/MODEL_2D_Multirotor.m
+++ model/MODEL_2D_Multirotor.m
@@ -3,13 +3,15 @@ classdef MODEL_2D_Multirotor
         q;
         pos;
         vel;
+        m;
     end
     methods
         function obj = MODEL_2D_Multirotor()
+            obj.m = 1.0;
             obj.q = 0.0;
         
             obj.pos = [0.0, 1.0];
-            obj.vel = [0.0, 0.0];
+            obj.vel = [1.0, 0.0];
             
         end
         
@@ -18,7 +20,7 @@ classdef MODEL_2D_Multirotor
             [position, attitude, velocity] = getstate(obj);
             
             % Calculate dynamics
-            acc = thrust  * [cos(attitude + pi()/2), sin(attitude + pi()/2)] + [0.0, -9.8];
+            acc = thrust/obj.m  * [cos(attitude + pi()/2), sin(attitude + pi()/2)] + [0.0, -9.8];
             vel = velocity + acc * dt;
             pos = position + velocity * dt + 0.5*acc*dt^2;
             q = attitude + w * dt;
@@ -30,16 +32,22 @@ classdef MODEL_2D_Multirotor
         end
         
         function [pos, q, vel] = getstate(obj)
-            pos = obj.pos
+            pos = obj.pos;
             q = obj.q;
             vel = obj.vel;
             
         end
         function visualize(obj)
-            [position, ~, ~] = getstate(obj);
-             plot(position(1), position(2), 'rx'); hold on;
-             drawnow
+            [position, attitude, ~] = getstate(obj);
+            arm_length = 0.5;
+            rotor1 = position + arm_length * [cos(attitude), sin(attitude)];
+            rotor2 = position - arm_length * [cos(attitude), sin(attitude)];
             
+            fuselarge = [rotor1; rotor2];
+            plot(position(1), position(2), 'rx'); hold on;
+            plot(fuselarge(:, 1), fuselarge(:, 2), 'r-'); 
+            xlim([-1.0, 1.0]); ylim([0, 2.0]); hold off;
+
         end
     end
 end
